cf8086125e917b609b891d73a38535854bab2c40
Archie Fielding <archiefielding@gmail.com>
2015-09-15 14:04:15 +0100

    Added name entering

--- BattleshipsWebMon/lib/battleships_web.rb
+++ BattleshipsWebMon/lib/battleships_web.rb
@@ -2,8 +2,19 @@ require 'sinatra/base'
 
 class BattleshipsWeb < Sinatra::Base
   get '/' do
-    'Hello BattleshipsWeb!'
+    erb :index
   end
+
+  get '/newGame' do
+    erb :newGame
+  end
+
+  get '/greetings' do
+    @visitor = params[:name]
+    erb :greetings
+  end
+
+set :views, proc { File.join(root, '..', 'views') }
 
   # start the server if ruby file executed directly
   run! if app_file == $0
---
+++ BattleshipsWebMon/spec/features/starting_a_game_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+feature 'Starting a new game' do
+  scenario 'I am asked to enter my name' do
+    visit '/'
+    click_link 'New Game'
+    expect(page).to have_content "What's your name?"
+  end
+
+  scenario 'entering my name takes me to a new page containing a greeting' do
+    visit '/newGame'
+    fill_in('name', :with => 'Archie')
+    click_button('Submit')
+    expect(page).to have_content "Greetings Archie!"
+  end
+
+  scenario 'not entering a name will ask for a name' do
+    visit '/newGame'
+    fill_in('name', :with => '')
+    click_button('Submit')
+    expect(page).to have_content "Please enter a name!"
+  end
+
+end
